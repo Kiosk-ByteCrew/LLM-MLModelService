4cbd5f67a280b98c4f8980166b4b03d0b28189c9
Aashna Arun <aashna@Aashnas-MacBook-Air.local>
2024-11-29 13:32:05 -0500

    File structure with db connection

--- app/api/v1/endpoints/health.py
+++ app/api/v1/endpoints/health.py
@@ -1,16 +1,24 @@
-from fastapi import APIRouter
-from pymongo.collection import Collection
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-#from app.db.mongo import collection
+from app.db.mongo import collection
+from pymongo.errors import ServerSelectionTimeoutError
+from app.core.config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME
 
-# Router for health check
 router = APIRouter()
 
-# Health check response model
 class HealthResponse(BaseModel):
     LLMName: str
 
 @router.get("/health", response_model=HealthResponse)
 async def health_check():
-    document = {"LLMName": "Application is up"}
-    return HealthResponse(**document)
+    try:
+        print(MONGO_URI)
+        print(DATABASE_NAME)
+        print(COLLECTION_NAME)
+        # Fetch a document from MongoDB
+        document = collection.find_one({}, {"_id": 0})
+        if document:
+            return HealthResponse(**document)
+        raise HTTPException(status_code=404, detail="Document not found")
+    except ServerSelectionTimeoutError as e:
+        raise HTTPException(status_code=500, detail=f"MongoDB connection error: {str(e)}")
